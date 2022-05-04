3db4848897fb76f9cac11b4c3b73ab3d9bfb9949
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-05-04 23:43:04 +0300

    s
    s

--- Web Calculator/script.js
+++ Web Calculator/script.js
@@ -1,76 +1,96 @@
 const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
-let display_value = '0';
-let first_value = null;
+let displayValue = '0';
+let firstValue = null;
 let operator = null;
-let waiting_for_second_value = false;
+let waitingForSecondValue = false;
 
-keys_value();
+updateDisplay();
 
-function keys_value() {
-    display.value = display_value;
+function updateDisplay() {
+    display.value = displayValue;
 }
-
 
 keys.addEventListener('click', function(e) {
     const element = e.target;
+    const value = element.value;
 
     if (!element.matches('button')) return;
 
-    if(element.classList.contains('operator')) {
-        // console.log('operator', element.value);
-        handle_operator(element.value);
-        return;
+    switch(value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            inputNumber(element.value);        
     }
-
-    if(element.classList.contains('decimal')) {
-        // console.log('decimal', element.value);
-        input_decimal();
-        keys_value();
-        return;
-    }
-
-    if(element.classList.contains('clear')) {
-        // console.log('clear', element.value);
-        clear();
-        keys_value();
-        return;
-    }
-
-    // console.log('number', element.value);
-    input_number(element.value);
-    keys_value();
+    updateDisplay();
 });
 
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
 
-function handle_operator(next_operator){
-    const value = parseFloat(display_value);
-
-    if(first_value === null){
-        first_value = value;
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
     }
 
-    waiting_for_second_value = true;
-    operator = next_operator;
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
-function input_number(num) {
-    
-    if(display_value = num){
-    waiting_for_second_value = false;
-
-} else {
-    display_value = display_value === '0'? num: display_value + num;
+function calculate(first, second, operator) {
+    if(operator === '+') {
+        return first + second;
+    } else if (operator === '-') {
+        return first - second;
+    } else if (operator === '*') {
+        return first * second
+    } else if (operator === '/') {
+        return first / second;
+    }
+    return second;
 }
+
+function inputNumber(num) {
+    if(waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === '0'? num: displayValue + num;
+    }
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
-function input_decimal() {
-    if (!display_value.includes('.')) {
-        display_value += '.';
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.';
     }
 }
 
 function clear() {
-    display_value = '0';
+    displayValue = '0';
 }
